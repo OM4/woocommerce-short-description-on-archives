a449e51e7b3bf0900e95fe845f019008ce918006
James Collins <james@om4.com.au>
2014-08-18 15:40:56 +0800

    Initial Commit

---
+++ readme.txt
@@ -0,0 +1,26 @@
+=== WooCommerce Product Short Descriptions on Shop Pages ===
+Contributors: jamescollins
+Tags: woocommerce, product, description, archive, category, shop
+Requires at least: 3.9
+Tested up to: 3.9.2
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Adds each WooCommerce product's short description to shop and category/tag archive pages.
+
+== Description ==
+
+Adds each WooCommerce product's short description to shop and category/tag archive pages.
+
+If you have any other suggestions for other information that could be included, please [add your suggestion via GitHub](https://github.com/OM4/woocommerce-short-description-on-archives/issues).
+
+== Installation ==
+
+1. Download the latest stable version of the plugin from https://github.com/OM4/woocommerce-short-description-on-archives/archive/release.zip.
+1. Install this plugin via WordPress Dashboard, Plugins, Add New, Upload.
+
+== Changelog ==
+
+= 0.1 =
+* Initial release.
---
+++ woocommerce-short-description-on-archives.php
@@ -0,0 +1,81 @@
+<?php
+/*
+Plugin Name: WooCommerce Product Short Descriptions on Shop Pages
+Plugin URI: https://github.com/OM4/woocommerce-short-description-on-archives
+Description: Adds each WooCommerce product's short description to shop and category/tag archive pages.
+Version: 0.1
+Author: OM4
+Author URI: http://om4.com.au/
+Text Domain: woocommerce-short-description-on-archives
+Git URI: https://github.com/OM4/woocommerce-short-description-on-archives
+Git Branch: release
+License: GPLv2
+*/
+
+/*
+Copyright 2014 OM4 (email: info@om4.com.au    web: http://om4.com.au/)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+if ( ! class_exists( 'WC_Short_Description_On_Archives' ) ) {
+
+	/**
+	 * This class is a singleton.
+	 *
+	 * Class WC_Short_Description_On_Archives
+	 */
+	class WC_Short_Description_On_Archives {
+
+		/**
+		 * Refers to a single instance of this class
+		 */
+		private static $instance = null;
+
+		/**
+		 * Creates or returns an instance of this class
+		 * @return WC_Short_Description_On_Archives A single instance of this class
+		 */
+		public static function instance() {
+			if ( null == self::$instance ) {
+				self::$instance = new self();
+			}
+			return self::$instance;
+
+		}
+
+		/**
+		 * Constructor
+		 */
+		private function __construct() {
+			add_action( 'woocommerce_after_shop_loop_item_title', array( $this, 'woocommerce_after_shop_loop_item_title' ), 5 );
+		}
+
+		/**
+		 * Add the product short description when displaying a product in the loop.
+		 *
+		 * Executed by the woocommerce_after_shop_loop_item_title hook.
+		 */
+		public function woocommerce_after_shop_loop_item_title( $classes, $class = '', $post_id = '' ) {
+
+			wc_get_template( 'single-product/short-description.php' );
+
+		}
+
+	}
+
+	WC_Short_Description_On_Archives::instance();
+
+}
